90c67f82afe1eb713c83c1d14c709f5cd18a881c
Seymaster <alugbinoluwaseyi1@gmail.com>
2020-11-27 12:44:43 +0100

    cache set

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const client = redis.createClient(REDIS_PORT);
 
 const app = express();
 
+function setResponse(username, repos){
+    return `<h2>${username} has ${repos} public repositories</h2>`
+}
 // Fetch repo from github
 async function getRepos(req,res,next){
     try {
@@ -16,13 +19,32 @@ async function getRepos(req,res,next){
         const { username } = req.params
         const response = await fetch(`https://api.github.com/users/${username}`)
         let data = await response.json()
-        res.send(data)
+        const repos = data.public_repos
+// set data to Redis
+        client.setex(username, 2400, repos)
+
+        res.send(setResponse(username, repos))
     } catch (err) {
         console.error(err)
         res.status(500)
     }
 }
-app.get("/repos/:username", getRepos);
+
+function cache(req,res,next){
+    const { username } = req.params;
+
+    client.get(username, (err, data)=>{
+        if (err) throw err;
+
+        if (data !== null){
+            res.send(setResponse(username, data))
+        } else{
+            next();
+        }
+    })
+};
+
+app.get("/repos/:username", cache ,getRepos);
 
 app.listen(PORT, ()=>{
     console.log(`app is listening on port ${PORT}`);
